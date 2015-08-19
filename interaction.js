76004de6fbeef3393cfb4e321693da1d7efa89a8
Kerry Halupka <kerry.halupka@gmail.com>
2015-08-19 14:37:05 +1000

    Updates for Shan

--- interaction.js
+++ interaction.js
@@ -21,3 +21,39 @@ function feed(){
 function run(){
 	cat_image.style.width=(cat_image.offsetWidth-30)+'px';
 }
+
+
+var cat_object = {
+    weight : 5,
+    past_weight_values : [4.5, 5.1, 4.9],
+    name : 'Princess Caroline'
+};
+var temp_object=[cat_object];
+cat_object.height=50;
+
+var cat_list=[cat_object];
+cat_list.push({weight : 6 , past_weight_values : [5.9, 5.3, 6.1], name : 'Snowball'});
+cat_list.push({past_weight_values : [5.9, 5.3, 6.1]});
+
+var cat_list2 = [cat_object, cat_object, temp_object];
+
+cat_list2[0].name='tinkerbell';
+// console.log(cat_list);
+console.log(cat_list2);
+
+var dog_list = cat_list.map(function(cat){
+	return {
+		name: "Mr"+cat.name
+	}
+})
+
+
+function dog_weight(cat_weight){
+
+	
+}
+var a={name: 'string1'};
+var b=a.name;
+a.name='string2';
+console.log(a);
+console.log(b);
